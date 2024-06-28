88e10547812f76600f956d507acb66d576670f8a
Gustavo Pierre <gustavopierre@gmail.com>
2024-06-28 01:21:43 +0100

    Choose fields on table

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 /*
   --------------------------------------------------------------------------------------
-  Função para obter a lista existente do servidor via requisição GET
+  Function to obtain the existing list from the server by GET request
   --------------------------------------------------------------------------------------
 */
 const getList = async () => {
@@ -10,10 +10,11 @@ const getList = async () => {
   })
     .then((response) => response.json())
     .then((data) => {
-      data.dataset.forEach(item => insertList(item.name, item.area, item.format, 
-        item.description, item.creator, item.source, item.permitted, item.copyright, 
-        item.link, item.info, item.coordinatesystem, item.creationdate, 
-        item.updatedate, item.updatedate, item.checkdate))
+      data.dataset.forEach(item => insertList(item.name, item.area, 
+        item.description, item.source, item.creator, item.permitted, 
+        item.copyright, item.link, item.info, item.coordinate_system, 
+        item.creation_date, item.update_date, item.check_date, 
+        item.format))
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -22,7 +23,7 @@ const getList = async () => {
 
 /*
   --------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos dados
+  Initial call of function to get the list from the server
   --------------------------------------------------------------------------------------
 */
 getList()
@@ -30,30 +31,30 @@ getList()
 
 /*
   --------------------------------------------------------------------------------------
-  Função para colocar um item na lista do servidor via requisição POST
+  Function to insert a new data in the server list by POST request
   --------------------------------------------------------------------------------------
 */
-const postItem = async (inputName, inputArea, inputFormat,
-  inputDescription, inputCreator, inputSource, inputPermitted, inputCopyRight,
-  inputLink, inputInfo, inputCoordinateSystem, inputCreationDate,
-  inputUpdateDate, inputCheckDate) => {
+const postData = async (inputName, inputArea, inputDescription, inputSource, 
+  inputCreator, inputPermitted, inputCopyright, inputLink, inputInfo,
+  inputCoordinateSystem, inputCreationDate, inputUpdateDate, 
+  inputCheckDate, inputFormat) => {
   const formData = new FormData();
   formData.append('name', inputName);
   formData.append('area', inputArea);
-  formData.append('format', inputFormat);
   formData.append('description', inputDescription);
-  formData.append('creator', inputCreator);
   formData.append('source', inputSource);
+  formData.append('creator', inputCreator);
   formData.append('permitted', inputPermitted);
-  formData.append('copyRight', inputCopyRight);
+  formData.append('copyright', inputCopyright);
   formData.append('link', inputLink);
   formData.append('info', inputInfo);
-  formData.append('coordinateSystem', inputCoordinateSystem);
-  formData.append('creationDate', inputCreationDate);
-  formData.append('updateDate', inputUpdateDate);
-  formData.append('checkDate', inputCheckDate);
+  formData.append('coordinate_system', inputCoordinateSystem);
+  formData.append('creation_date', inputCreationDate);
+  formData.append('update_date', inputUpdateDate);
+  formData.append('check_date', inputCheckDate);
+  formData.append('format', inputFormat);
 
-  let url = 'http://127.0.0.1:5000/produto';
+  let url = 'http://127.0.0.1:5000/data';
   fetch(url, {
     method: 'post',
     body: formData
@@ -91,10 +92,10 @@ const removeElement = () => {
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       let div = this.parentElement.parentElement;
-      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      const dataName = div.getElementsByTagName('td')[0].innerHTML
       if (confirm("Are you sure?")) {
         div.remove()
-        deleteItem(nomeItem)
+        deleteItem(dataName)
         alert("Deleted!")
       }
     }
@@ -120,73 +121,98 @@ const deleteItem = (name) => {
 
 /*
   --------------------------------------------------------------------------------------
-  Função para adicionar um novo item com nome, quantidade e valor 
+  Function to add a new item with name, area, format, description, 
+  creator, source, permitted, copyRight, link, info, coordinateSystem, creationDate, updateDate, checkDate
   --------------------------------------------------------------------------------------
 */
 const newData = () => {
-  let inputProduct = document.getElementById("newData").value;
-  let inputQuantity = document.getElementById("newFormat").value;
-  let inputPrice = document.getElementById("newDescription").value;
-  let inputCreator = document.getElementById("newCreator").value;
+  let inputName = document.getElementById("newName").value;
+  let inputArea = document.getElementById("newArea").value;
+  let inputDescription = document.getElementById("newDescription").value;
   let inputSource = document.getElementById("newSource").value;
+  let inputCreator = document.getElementById("newCreator").value;
   let inputPermitted = document.getElementById("newPermitted").value;
-  let inputCopyRight = document.getElementById("newCopyRight").value;
+  let inputCopyright = document.getElementById("newCopyright").value;
   let inputLink = document.getElementById("newLink").value;
   let inputInfo = document.getElementById("newInfo").value;
   let inputCoordinateSystem = document.getElementById("newCoordinateSystem").value;
   let inputCreationDate = document.getElementById("newCreationDate").value;
   let inputUpdateDate = document.getElementById("newUpdateDate").value;
   let inputCheckDate = document.getElementById("newCheckDate").value;
+  let inputFormat = document.getElementById("newFormat").value;
   
-  if (inputData === '') {
-    alert("Write a data!");
-  } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
-    alert("Quantidade e valor precisam ser números!");
+  if (inputName === '') {
+    alert("Write the data name!");
+  } else if (inputFormat === '') {
+    alert("Write the data format");
   } else {
-    insertList(inputData, inputFormat, inputCreator, inputSource, 
-      inputPermitted, inputCopyRight, inputLink, inputInfo, 
+    insertList(inputName, inputArea, inputDescription, inputSource, 
+      inputCreator, inputPermitted, inputCopyright, inputLink, inputInfo,
       inputCoordinateSystem, inputCreationDate, inputUpdateDate, 
-      inputCheckDate)
-    postItem(inputData, inputFormat, inputCreator, inputSource, 
-      inputPermitted, inputCopyRight, inputLink, inputInfo, 
+      inputCheckDate, inputFormat)
+    postData(inputName, inputArea, inputDescription, inputSource, 
+      inputCreator, inputPermitted, inputCopyright, inputLink, inputInfo,
       inputCoordinateSystem, inputCreationDate, inputUpdateDate, 
-      inputCheckDate)
+      inputCheckDate, inputFormat)
     alert("Data added!")
   }
 }
 
 /*
   --------------------------------------------------------------------------------------
-  Função para inserir items na lista apresentada
+  Function to insert data information in showed list
   --------------------------------------------------------------------------------------
 */
-const insertList = (name, format,description, creator,
-  source, permitted, copyRight, link, info, coordinateSystem,
-  creationDate, updateDate, checkDate) => {
-  var item = [name, format, description, creator, source, 
-    permitted, copyRight, link, info, coordinateSystem, 
-    creationDate, updateDate, checkDate]
+const insertList = (name, area, description, source, creator, permitted, 
+  copyright, link, info, coordinateSystem, creationDate, updateDate, 
+  checkDate, format) => {
+  var data = [name, area, permitted, coordinateSystem, checkDate, format]
+  // Supondo que checkDate seja uma string no formato ISO 8601 (YYYY-MM-DDTHH:MM:SSZ)
+  var checkDate = data[4]; // Acessando a variável checkDate do array
+
+  // 1. Converter checkDate para objeto Date
+  var checkDateObj = new Date(checkDate);
+
+  // 2. Obter a data atual
+  var currentDate = new Date();
+
+  // 3. Calcular a diferença em milissegundos
+  var differenceInMilliseconds = currentDate - checkDateObj;
+
+  // 4. Converter a diferença para dias
+  var differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+
+  // Ajustar para arredondar para cima se for mais que meio dia
+  var differenceInDaysAdjusted = differenceInDays - Math.floor(differenceInDays) > 0.5 ? Math.ceil(differenceInDays) : Math.floor(differenceInDays);
+
+  // 5. Verificar o número de dias para definir a unidade
+  var dayOrDays = differenceInDaysAdjusted <= 1 ? "day" : "days";
+
+  // 6. Atualizar o valor de checkDate com a diferença e a unidade
+  data[4] = `${differenceInDaysAdjusted} ${dayOrDays}`;
+
   var table = document.getElementById('myTable');
   var row = table.insertRow();
   
-  for (var i = 0; i < item.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     var cel = row.insertCell(i);
-    cel.textContent = item[i];
+    cel.textContent = data[i];
   }
   insertButton(row.insertCell(-1))
-  document.getElementById("newData").value = "";
-  document.getElementById("newFormat").value = "";
+  document.getElementById("newName").value = "";
+  document.getElementById("newArea").value = "";
   document.getElementById("newDescription").value = "";
-  document.getElementById("newCreator").value = "";
   document.getElementById("newSource").value = "";
+  document.getElementById("newCreator").value = "";
   document.getElementById("newPermitted").value = "";
-  document.getElementById("newCopyRight").value = "";
+  document.getElementById("newCopyright").value = "";
   document.getElementById("newLink").value = "";
   document.getElementById("newInfo").value = "";
   document.getElementById("newCoordinateSystem").value = "";
   document.getElementById("newCreationDate").value = "";
   document.getElementById("newUpdateDate").value = "";
   document.getElementById("newCheckDate").value = "";
+  document.getElementById("newFormat").value = "";
   
   removeElement()
 }
